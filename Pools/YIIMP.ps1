558bc84db6da906e00e2dd921c0195ef15487b76
nmgrilo <nuno.grilo@gmail.com>
2018-03-06 01:44:48 +0000

    Fix timeout. Eliminate nonexistant coins

--- Pools/YIIMP.ps1
+++ Pools/YIIMP.ps1
@@ -52,7 +52,7 @@ if ($Querymode -eq "info"){
                             
                             try {
                                 $http="http://api.yiimp.eu/api/wallet?address="+$Info.user
-                                $Request = Invoke-WebRequest -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"  $http -UseBasicParsing -timeoutsec 10 | ConvertFrom-Json 
+                                $Request = Invoke-WebRequest -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"  $http -UseBasicParsing -TimeoutSec 10 | ConvertFrom-Json 
                             }
                             catch {}
         
@@ -80,7 +80,7 @@ if ($Querymode -eq "speed")    {
                             
     try {
         $http="http://api.yiimp.eu/api/walletEx?address="+$Info.user
-        $Request = Invoke-WebRequest -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"  $http -UseBasicParsing -timeoutsec 5 | ConvertFrom-Json 
+        $Request = Invoke-WebRequest -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"  $http -UseBasicParsing -TimeoutSec 5 | ConvertFrom-Json 
     }
     catch {}
     
@@ -114,9 +114,9 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
         $retries=1
                 do {
                         try {
-                            $Request = Invoke-WebRequest "http://api.yiimp.eu/api/currencies"  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36" -UseBasicParsing -timeout 5  | ConvertFrom-Json 
+                            $Request = Invoke-WebRequest "http://api.yiimp.eu/api/currencies"  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36" -UseBasicParsing -TimeoutSec 5  | ConvertFrom-Json 
                             start-sleep 5
-                            $Request2 = Invoke-WebRequest "http://api.yiimp.eu/api/status"  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36" -UseBasicParsing -timeout 5 | ConvertFrom-Json  
+                            $Request2 = Invoke-WebRequest "http://api.yiimp.eu/api/status"  -UserAgent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36" -UseBasicParsing -TimeoutSec 5 | ConvertFrom-Json  
 
                         }
                         catch {}
@@ -138,37 +138,39 @@ if (($Querymode -eq "core" ) -or ($Querymode -eq "Menu")){
                     $Yiimp_Algorithm = get_algo_unified_name $coin.algo
                     $Yiimp_coin =   get_coin_unified_name $coin.name
                     $Yiimp_Symbol=$_.name
+					
+					if ($CoinsWallets.get_item($Yiimp_Symbol) -ne $null) {
             
 
-                    $Divisor = Get_Algo_Divisor $Yiimp_Algorithm
-                    
-                
-                    $Result+=[PSCustomObject]@{
-                                Algorithm     = $Yiimp_Algorithm
-                                Info          = $Yiimp_coin
-                                Price         = [Double]$coin.estimate / $Divisor
-                                Price24h      = [Double]$coin.estimate_last24h  / $Divisor
-                                Protocol      = "stratum+tcp"
-                                Host          = "yiimp.eu"
-                                Port          = $coin.port
-                                User          = $CoinsWallets.get_item($Yiimp_Symbol)
-                                Pass          = "c=$Yiimp_symbol,ID=#WorkerName#"
-                                Location      = 'US'
-                                SSL           = $false
-                                Symbol        = $Yiimp_Symbol
-                                AbbName       = $AbbName
-                                ActiveOnManualMode    = $ActiveOnManualMode
-                                ActiveOnAutomaticMode = $ActiveOnAutomaticMode
-                                PoolWorkers       = $coin.Workers
-                                PoolHashRate  = $coin.HashRate
-                                Blocks_24h    = $coin."24h_blocks"
-                                WalletMode    = $WalletMode
-                                Walletsymbol = $Yiimp_Symbol
-                                PoolName = $Name
-                                Fee = ($Request2.($coin.algo).Fees)/100
-                                RewardType=$RewardType
-                                }
-                        
+						$Divisor = Get_Algo_Divisor $Yiimp_Algorithm
+						
+					
+						$Result+=[PSCustomObject]@{
+									Algorithm     = $Yiimp_Algorithm
+									Info          = $Yiimp_coin
+									Price         = [Double]$coin.estimate / $Divisor
+									Price24h      = [Double]$coin.estimate_last24h  / $Divisor
+									Protocol      = "stratum+tcp"
+									Host          = "yiimp.eu"
+									Port          = $coin.port
+									User          = $CoinsWallets.get_item($Yiimp_Symbol)
+									Pass          = "c=$Yiimp_symbol,ID=#WorkerName#"
+									Location      = 'US'
+									SSL           = $false
+									Symbol        = $Yiimp_Symbol
+									AbbName       = $AbbName
+									ActiveOnManualMode    = $ActiveOnManualMode
+									ActiveOnAutomaticMode = $ActiveOnAutomaticMode
+									PoolWorkers       = $coin.Workers
+									PoolHashRate  = $coin.HashRate
+									Blocks_24h    = $coin."24h_blocks"
+									WalletMode    = $WalletMode
+									Walletsymbol = $Yiimp_Symbol
+									PoolName = $Name
+									Fee = ($Request2.($coin.algo).Fees)/100
+									RewardType=$RewardType
+									}
+					}
                 
                 }
 
